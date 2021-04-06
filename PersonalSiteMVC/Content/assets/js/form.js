53d64fa785ff9e27938c9bd39cd7585a99937494
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-04-06 17:55:44 -0500

    Add project files.

---
+++ PersonalSiteMVC/Content/assets/js/form.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	
+	    
+    /* ======= jQuery form validator ======= */ 
+    /* Ref: http://jqueryvalidation.org/documentation/ */   
+    $("#contact-form").validate({
+		messages: {
+		
+		    name: {
+    			required: 'Please enter your name'
+			},
+			email: {
+				required: 'Please enter your email'
+			},			
+			message: {
+				required: 'Please enter your message'
+			}
+			
+		}
+		
+	});
+    
+
+});
