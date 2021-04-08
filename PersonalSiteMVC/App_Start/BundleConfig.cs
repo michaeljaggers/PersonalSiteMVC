8e89c96e8225677b1aab74e13cedd68a4ade76b6
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-04-08 17:42:19 -0500

    Cleanup bundle config

--- PersonalSiteMVC/App_Start/BundleConfig.cs
+++ PersonalSiteMVC/App_Start/BundleConfig.cs
@@ -8,7 +8,6 @@ namespace PersonalSiteMVC
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-
             bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
                         "~/Content/assets/plugins/jquery-3.4.1.min.js",
                         "~/Content/assets/plugins/popper.min.js",
@@ -22,18 +21,6 @@ namespace PersonalSiteMVC
                         "~/Scripts/jquery.unobtrusive-ajax.min.js",
                         "~/Content/assets/plugins/jquery-validation/jquery.validate.min.js",
                         "~/Content/assets/js/form.js"));
-
-                              // Use the development version of Modernizr to develop with and learn from. Then, when you're
-                              // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-                              bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
